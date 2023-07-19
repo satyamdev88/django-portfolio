8e7b0fc090c1904183f15d9971f0653b3d84a3bf
satyamdev88 <128727378+satyamdev88@users.noreply.github.com>
2023-07-20 00:20:49 +0530

    Update static file url and template url

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,"templates"],
+        'DIRS': [BASE_DIR,"Portfolio/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,5 +125,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR,"static",
+    BASE_DIR,"Portfolio/static",
 ]
